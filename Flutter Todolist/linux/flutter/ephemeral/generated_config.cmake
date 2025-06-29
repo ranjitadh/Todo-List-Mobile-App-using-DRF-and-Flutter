f28ed93dda57f5b0747145ab57f110073c93f249
ranjitadh <ranzeet60@gmail.com>
2025-06-29 23:54:12 +0545

    Update get resquest integration with app

---
+++ Flutter Todolist/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ranjit/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ranjit/Desktop/API/Flutter Todolist" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ranjit/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/ranjit/Desktop/API/Flutter Todolist"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ranjit/Desktop/API/Flutter Todolist/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ranjit/Desktop/API/Flutter Todolist/lib/main.dart"
+)
