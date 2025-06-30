a4ddc414f39d10748196ca96e97ae3cc437eb01c
ranjit <ranzeet60@gmail.com>
2025-06-30 21:19:13 +0545

    changes

--- Backend/ourtodoapp/settings.py
+++ Backend/ourtodoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zi4+9$07exx9m^*f5ivz&l0btrl7b37t4$*@&j#)j^*i57%&%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'todolist',
+    'corsheaders',  
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
